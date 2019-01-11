bd077dc32a65be43c28932676be83b930d4b56d6
Carlos Quiroz <cquiroz@gemini.edu>
2019-01-11 09:03:21 -0300

    wip
    Signed-off-by: Carlos Quiroz <cquiroz@gemini.edu>

--- modules/seqexec/server/src/main/scala/seqexec/server/SeqexecEngine.scala
+++ modules/seqexec/server/src/main/scala/seqexec/server/SeqexecEngine.scala
@@ -851,7 +851,7 @@ object SeqexecEngine extends SeqexecConfiguration {
     case UpdateQueueClear(qid)              => QueueUpdated(QueueManipulationOp.Clear(qid), svs)
     case StartQueue(qid, _)                 => QueueUpdated(QueueManipulationOp.Started(qid), svs)
     case StopQueue(qid, _)                  => QueueUpdated(QueueManipulationOp.Stopped(qid), svs)
-    case StartSysConfig(_, _, _)            => NullEvent
+    case StartSysConfig(sid, _, _)          => SingleActionEvent(SingleActionOp.Started(sid))
   }
 
   def toSeqexecEvent(ev: executeEngine.ResultType)(svs: => SequencesQueue[SequenceView]): SeqexecEvent = ev match {
